9c1df37b0084ba010b702accd8c6c2ea0acb2387
Luke Patterson <lukepatterson31@gmail.com>
2022-04-22 16:57:16 +0100

    added load testing scenario

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-
- 
 export const options = {
-    stages: [
-        // { duration: '30s', target: 20 },
-        // { duration: '1m30s', target: 10 },
-        { duration: '3s', target: 10 },
-    ],
+    scenarios: {
+        load_testing: {
+            executor: 'ramping-arrival-rate',
+            preAllocatedVUs: 5,
+            startRate: 1,
+            timeUnit: '1s',
+            maxVUs: 20,
+            stages: [
+              { duration: '30s', target: 10 },
+              { duration: '1m', target: 20 },
+              { duration: '30s', target: 10}
+            ]      
+        }        
+    }
 };
-
 export function setup() {
-var creds = getCredentials();
-   
+    var creds = getCredentials();
     return creds;
 }
-
 export default function (data) {
-   
-const params = {
+    const params = {
         headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Bearer ' + data.access_token,
+            'User-Agent': 'PostmanRuntime/7.29.0',
             'Accept': '*/*',
             'Cache-Control': 'no-cache',
             'Connection': 'keep-alive'
-        },
-};
-    const body = 'query=test@email.com+8.8.8.8&transactiontypeid=2';
-
-    const res = http.post('http://<machine ip>:5001/emailagevalidator/v2', body, params);
-     
+        }
+    };
+    const body = 'query=dev@test.com+8.8.8.8&transactiontypeid=2';
+    const res = http.post('https://localhost/emailagevalidator/v2', body, params);
     check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
 }
-
 function getCredentials() {
-
-    const url = '<CREDENTIALS_URL>';
+    const url = 'https://localhost/oauth/v2/token';
     const params = {
         headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/x-www-form-urlencoded',
             'Accept': '*/*',
             'Cache-Control': 'no-cache',
             'Connection': 'keep-alive'
         },
     };
-
-    var data = "grant_type=client_credentials&client_id=<CLIENT_ID>&client_secret=<CLIENT_SECRET>";
- 
+    var data = "grant_type=client_credentials&client_id=<SENSITIVE-DATA>>&client_secret=<SENSITIVE_DATA>>";
     var res = http.post(url, data, params);
-
-    return JSON.parse(res.body);;
+    return JSON.parse(res.body);
 }
